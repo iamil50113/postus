ae455ebe88e2bccddc353fc7710e864231adc60c
Ivan Lebedev <iamil50113@gmail.com>
2024-06-26 16:14:26 +0300

    first commit

--- internal/service/post/post.go
+++ internal/service/post/post.go
@@ -2,7 +2,6 @@ package post
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"postus/internal/domain/model"
 	"time"
@@ -47,12 +46,9 @@ func (p *Post) Posts(ctx context.Context) ([]*model.Post, error) {
 }
 
 func (p *Post) PostsForUser(ctx context.Context, uid int64) ([]*model.Post, error) {
-	user, err := p.usrProvider.User(ctx, uid)
+	_, err := p.usrProvider.User(ctx, uid)
 	if err != nil {
 		return nil, err
-	}
-	if user == nil {
-		return nil, fmt.Errorf("Invalid user id")
 	}
 	return p.postProvider.PostsForUserID(ctx, uid)
 }
@@ -61,9 +57,6 @@ func (p *Post) Post(ctx context.Context, id int64) (*model.Post, error) {
 	post, err := p.postProvider.Post(ctx, id)
 	if err != nil {
 		return nil, err
-	}
-	if post == nil {
-		return nil, fmt.Errorf("Invalid post id")
 	}
 	return post, err
 }
